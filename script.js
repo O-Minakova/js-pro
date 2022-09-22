b3e94ac43f214b0604b3df21651244ea65bb1a1d
O-Minakova <6ten198@mail.ru>
2022-09-22 23:59:42 +0300

    Commented unused Classes and methods

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
 const GOODS = "/catalogData.json";
 
-function maskGETRequest(url) {
+function makeGETRequest(url) {
   return new Promise((resolve) => {
     const xhr = new XMLHttpRequest();
     xhr.onload = function () {
@@ -37,7 +37,7 @@ class GoodsList {
 
   fetchGoods() {
     return new Promise((resolve) => {
-      maskGETRequest(`${BASE_URL}${GOODS}`).then((data) => {
+      makeGETRequest(`${BASE_URL}${GOODS}`).then((data) => {
         this.items = data;
         resolve();
       });
@@ -49,66 +49,66 @@ class GoodsList {
       this.items.map(item => new GoodsItem(item).render()).join('');
   }
 
-  getGoodsTotalPrice() {
-    return this.items.reduce((acc, item) => acc + item.price, 0);
-  }
+  // getGoodsTotalPrice() {
+  //   return this.items.reduce((acc, item) => acc + item.price, 0);
+  // }
 }
 
-class CartItem {
-  constructor({id, title, price}, count) {
-    this.id = id;
-    this.title = title;
-    this.price = price;
-    this.count = count;
-  }
+// class CartItem {
+//   constructor({id, title, price}, count) {
+//     this.id = id;
+//     this.title = title;
+//     this.price = price;
+//     this.count = count;
+//   }
+//
+//   getTotalPrice() {
+//     return this.price * this.count;
+//   }
+//
+//   renderItem() {
+//     // добавить код отрисовки элемента в Корзине
+//   }
+// }
 
-  getTotalPrice() {
-    return this.price * this.count;
-  }
-
-  renderItem() {
-    // добавить код отрисовки элемента в Корзине
-  }
-}
-
-class Cart {
-  constructor() {
-    this.items = new Map();
-  }
-
-  addItem({id, title, price}) {
-    if (!this.items.has(id)) {
-      this.items.set(id, new CartItem({id, title, price}, 0));
-    }
-    this.items.get(id).count++;
-    //добавить код добавления элемента в html кода корзины
-  }
-
-  removeItem(id) {
-    const cartItem = this.items.get(id);
-    if (cartItem.count === 1) {
-      this.items.delete(id);
-      //добавить код удаления элемента из html кода корзины
-    } else {
-      cartItem.count--;
-      //добавить код обновления элемента в html коде корзины
-    }
-    //добавить код обновления полной стоимости в html коде корзины
-  }
-
-  render() {
-    // добавить код отрисовки/отображения содержимого корзины при нажатии на кнопку Корзина
-  }
-
-  getCartTotalPrice() {
-    return [...this.items.values()]
-      .reduce((acc, item) => acc + item.getTotalPrice(), 0);
-  }
-
-  getCartProductsCount() {
-    return [...this.items.values()].reduce((acc, item) => acc + item.count, 0);
-  }
-}
+// class Cart {
+//   constructor() {
+//     this.items = new Map();
+//   }
+//
+//   addItem({id, title, price}) {
+//     if (!this.items.has(id)) {
+//       this.items.set(id, new CartItem({id, title, price}, 0));
+//     }
+//     this.items.get(id).count++;
+//     //добавить код добавления элемента в html кода корзины
+//   }
+//
+//   removeItem(id) {
+//     const cartItem = this.items.get(id);
+//     if (cartItem.count === 1) {
+//       this.items.delete(id);
+//       //добавить код удаления элемента из html кода корзины
+//     } else {
+//       cartItem.count--;
+//       //добавить код обновления элемента в html коде корзины
+//     }
+//     //добавить код обновления полной стоимости в html коде корзины
+//   }
+//
+//   render() {
+//     // добавить код отрисовки/отображения содержимого корзины при нажатии на кнопку Корзина
+//   }
+//
+//   getCartTotalPrice() {
+//     return [...this.items.values()]
+//       .reduce((acc, item) => acc + item.getTotalPrice(), 0);
+//   }
+//
+//   getCartProductsCount() {
+//     return [...this.items.values()].reduce((acc, item) => acc + item.count, 0);
+//   }
+// }
 
 const goodsList = new GoodsList();
 goodsList.fetchGoods().then(() => goodsList.render());
